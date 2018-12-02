37e2b033e3e825e0d575c714d41bc2ebe0b230dc
Ariel Juliet <arieljuliet@knights.ucf.edu>
2018-12-01 19:08:49 -0500

    Final Project

---
+++ FinalProject_TheFormula/Assets/control.cs
@@ -0,0 +1,31 @@
+﻿
+
+using System.Collections;
+using UnityEngine;
+
+public class control : MonoBehaviour
+{
+
+    public Transform sparkle;
+    void Start()
+    {
+        sparkle.GetComponent<ParticleSystem>().enableEmission = false;
+    }
+
+    void onTriggerEnter2D()
+    {
+        sparkle.GetComponent<ParticleSystem>().enableEmission = true;
+        StartCoroutine(stopSparkles());
+    }
+
+
+    IEnumerator stopSparkles()
+
+    {
+        yield return new WaitForSeconds (.4f);
+
+        sparkle.GetComponent<ParticleSystem>().enableEmission = false;
+
+    }
+
+}
